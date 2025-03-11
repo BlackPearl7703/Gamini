c18b34f6bc7581f6508d94d3c98306e50124e3f7
Prince Koshti <prince.koshti@cimpress.com>
2025-03-12 02:15:26 +0530

    interface change

--- index.js
+++ index.js
@@ -1,14 +1,12 @@
 let slideIndex = 0;
-showSlides();
-
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+// showSlides();
+window.addEventListener("scroll", function () {
+  const navbar = document.querySelector(".navbar");
+  if (window.scrollY > 50) { 
+    // console.log("hello maiya ")// Adjust threshold as needed
+      navbar.classList.add("scrolled");
+  } else {
+      navbar.classList.remove("scrolled");
   }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}
-  slides[slideIndex-1].style.display = "block";
-  setTimeout(showSlides, 2000); // Change image every 2 seconds
-} 
+});
+
